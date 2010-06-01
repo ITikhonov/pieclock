7a7a0d3bba2e3188198340207b96e821e333b568
kef <kefeer@brokestream.com>
2010-06-01 22:30:47 +0600

    sectioned and colored

--- PieClockScreenlet.py
+++ PieClockScreenlet.py
@@ -5,6 +5,7 @@ from screenlets import Screenlet
 from screenlets.options import IntOption, BoolOption, TimeOption, FloatOption
 from screenlets.options import StringOption, FontOption, ColorOption
 from screenlets.services import ScreenletService
+from cairo import OPERATOR_DEST_OUT,OPERATOR_OVER
 
 from math import pi
 from datetime import datetime
@@ -14,7 +15,8 @@ def deg(a): return a*(pi/180)
 
 def pie(ctx,a,r,c):
 	a=deg(a[0]-90),deg(a[1]-90)
-	ctx.set_source_rgb(*c)
+	if len(c)==3: ctx.set_source_rgb(*c)
+	else: ctx.set_source_rgba(*c)
 	ctx.arc(0,0,r[0], a[0],a[1])
 	ctx.arc_negative(0,0,r[1], a[1],a[0])
 	ctx.fill()
@@ -44,19 +46,35 @@ class PieClockScreenlet (Screenlet):
 		ctx.scale(self.width/2,self.height/2)
 		ctx.translate(1,1)
 
+		self._color_wd = (0.5,0.0,1.0)
+		self._color_hr = (0.0,0.5,1.0)
+		self._color_mn = (0.0,0.5,0.0)
+		self._color_sc = (0.8,0.8,0.8)
+
 		now=datetime.now()
 
+		wl=360/7.0
+		for i in range(0,7):
+			pie(ctx,(wl*i,wl*i+wl-4),(0.8,0.9),self._color_wd)
+		ctx.set_operator(OPERATOR_DEST_OUT)
+		pie(ctx,(0,wl*now.weekday()+(wl-4)*(now.hour/24.0+now.minute/(24*60.0))),(0.7,1),(1.0,1.0,1.0,0.8))
 
-		pie(ctx,(15*(now.hour+now.minute/60.0),360),(0.7,0.9),(0.0,1.0,0.0))
-		pie(ctx,(6*(now.minute+now.second/60.0),360),(0.4,0.6),(0.0,1.0,0.0))
-		pie(ctx,(6*(now.second+now.microsecond/1000000.0),360),(0.1,0.3),(0.0,1.0,0.0))
+		ctx.set_operator(OPERATOR_OVER)
+		for i in range(0,24):
+			pie(ctx,(15*i,15*i+11),(0.6,0.7),self._color_hr)
+		ctx.set_operator(OPERATOR_DEST_OUT)
+		pie(ctx,(0,15*now.hour+11*(now.minute/60.0)),(0.5,0.8),(0.0,1.0,0.0,0.8))
 
+		ctx.set_operator(OPERATOR_OVER)
+		for i in range(0,3):
+			pie(ctx,(120*i,120*i+110),(0.3,0.5),self._color_mn)
+		ctx.set_operator(OPERATOR_DEST_OUT)
+		pie(ctx,(0,120*(now.minute/20)+(110/20.0)*((now.minute%20)+now.second/60.0)),(0.2,0.6),(0.0,1.0,0.0,0.8))
 
-			
-# If the program is run directly or passed as an argument to the python
-# interpreter then create a Screenlet instance and show it
+		ctx.set_operator(OPERATOR_OVER)
+		pie(ctx,(6*(now.second+now.microsecond/1000000.0),360),(0.1,0.2),self._color_sc)
+
 if __name__ == "__main__":
-	# create new session
 	import screenlets.session
 	screenlets.session.create_session(PieClockScreenlet)
 
